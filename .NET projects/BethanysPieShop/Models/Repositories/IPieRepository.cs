09c53dfa18af92787f5c185f4c5212ef985d5c1b
Pavel Nurgaliev <pava62nv@gmail.com>
2026-02-27 15:59:19 +0300

    [Pavel-Nurgaliev] Changes into UbsBgContractFrm

--- .NET projects/BethanysPieShop/Models/Repositories/IPieRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BethanysPieShop.Models.Repositories
-{
-    public interface IPieRepository
-    {
-        IEnumerable<Pie> AllPies { get; }
-        IEnumerable<Pie> PiesOfTheWeek { get; }
-        Pie? GetPieById(int id);
-    }
-}
