146bac5d1f54069add881bbc20820e5dfbd389bb
Pavel Nurgaliev <pava62nv@gmail.com>
2026-04-07 12:11:01 +0300

    [Pavel-Nurgaliev] Renamed main form project

--- UnComProjects/UBS_CLIENT/UBS/FRM/PS/UbsPsUtPaymentGroupFrm_CP/UbsPsUtPaymentGroupFrm/UbsPsUtPaymentGroupFrm.Constants.cs
+++ UnComProjects/UBS_CLIENT/UBS/FRM/PS/UbsPsUtPaymentGroupFrm_CP/UbsPsUtPaymentGroupFrm/UbsPsUtPaymentGroupFrm.Constants.cs
@@ -1,6 +1,6 @@
 namespace UbsBusiness
 {
-    public partial class UbsPsUtPaymentGroupFrm : UbsFormBase
+    public partial class UbsPsUtPaymentGroupFrm
     {
         private const string LoadResource = @"VBS:UBS_VBD\PS\UT\UtPaymentF.vbs";
     }
