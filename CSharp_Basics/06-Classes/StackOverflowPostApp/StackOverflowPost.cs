fbd76a57c75936ed030afad74a7514e156ded115
Pavel Nurgaliev <pava62nv@gmail.com>
2026-06-21 21:46:40 +0300

    [Pavel-Nurgaliev] SStackOverflow Posts project task is completed

---
+++ CSharp_Basics/06-Classes/StackOverflowPostApp/StackOverflowPost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflowPostApp
+{
+    internal class StackOverflowPost
+    {
+        public StackOverflowPost(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            DateCreated = DateTime.Now;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public int Vote { get; private set; }
+
+        public void UpVote()
+        {
+            Vote++;
+        }
+        public void DownVote()
+        {
+            Vote--;
+        }
+    }
+}
