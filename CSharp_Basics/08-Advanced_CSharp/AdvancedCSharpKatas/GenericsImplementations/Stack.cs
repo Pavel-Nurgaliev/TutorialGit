22ab2b4f0f20eb34c281153fc19f02a141303f87
Pavel Nurgaliev <pava62nv@gmail.com>
2026-07-13 22:42:28 +0300

    Implemented kata 8

---
+++ CSharp_Basics/08-Advanced_CSharp/AdvancedCSharpKatas/GenericsImplementations/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace GenericsImplementations
+{
+    //making <T> class frees us to using object and perform box/ubox operations on them
+    //IEnumerable<T> allow program to interact with class as it collection of something
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int DefaultCapacity = 4;
+
+        private T[] _items = new T[DefaultCapacity];
+        private int _count = 0;
+
+        public void Push(T item)
+        {
+            if (_count > _items.Length)
+            {
+                Array.Resize<T>(ref _items, _items.Length * 2);
+            }
+
+            _items[_count++] = item;
+        }
+        public T Pop()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack empty");
+            }
+
+            var item = _items[--_count];
+            _items[_count] = default;
+
+            return item;
+        }
+        public T Peek()
+        {
+            return _items[_count - 1];
+        }
+        public int Count()
+        {
+            return _count;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharp_Basics/08-Advanced_CSharp/AdvancedCSharpKatas/KatasTests/StackTests.cs
@@ -0,0 +1,35 @@
+﻿namespace KatasTests
+{
+    internal class StackTests
+    {
+        GenericsImplementations.Stack<int> _stack = new GenericsImplementations.Stack<int>();
+        public StackTests()
+        {
+            FulfillStack(_stack);
+        }
+        [Test]
+        public void PeekTest_FulfilledStack_PeekItem()
+        {
+            Assert.That(_stack.Peek(), Is.EqualTo(5));
+        }
+        [Test]
+        public void PopTest_FulfilledStack_PeekItem()
+        {
+            Assert.That(_stack.Pop(), Is.EqualTo(5));
+        }
+        [Test]
+        public void CountTest_FulfilledStack_PeekItem()
+        {
+            Assert.That(_stack.Count(), Is.EqualTo(3));
+        }
+        [Test]
+        public void StackEnumerableTest_FulfilledStack_StringResult()
+        {
+            Assert.That(string.Join(",", _stack), Is.EqualTo("5,2,1"));
+        }
+        private void FulfillStack(GenericsImplementations.Stack<int> stack)
+        {
+            stack.Push(1); stack.Push(2); stack.Push(5);
+        }
+    }
+}
