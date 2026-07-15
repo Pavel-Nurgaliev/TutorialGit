3fe11b63c57d4456d205d365a5b45788a2ed753c
Pavel Nurgaliev <pava62nv@gmail.com>
2026-07-15 22:06:32 +0300

    implemented kata 9

---
+++ CSharp_Basics/08-Advanced_CSharp/AdvancedCSharpKatas/GenericsImplementations/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsImplementations
+{
+    public static class Helper
+    {
+        //where T : IComparable<T> constraits that our generic type has an implementatiuon if IComparable<T> interface
+        public static T Max<T>(IEnumerable<T> items) where T : IComparable<T>
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException($"Items {nameof(items)} are null");
+            }
+
+            T max = default!;
+            bool hasValue = false;
+
+            foreach (var item in items)
+            {
+                if (!hasValue || item.CompareTo(max) > 0)
+                {
+                    max = item;
+                    hasValue = true;
+                }
+            }
+
+            if (!hasValue)
+            {
+                throw new InvalidOperationException("Sequence is empty");
+            }
+
+            return max;
+        }
+    }
+}
