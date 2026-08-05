ea97ee081ad8257e219d632a08e3c61c7a0a36aa
Pavel-Nurgaliev <pava62nv@gmial.com>
2026-08-05 20:27:25 +0300

    Deleted unused project

--- CSharp_Basics/08-Advanced_CSharp/ConsoleApp2/Class2.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsoleApp2
-{
-    [Flags]
-    public enum DaysOfTheWeek { Mon = 1, Thue = 2, Wedn = 3, Thur = 4, Fri = 5, Sat = 6, Sun = 7, Weekends = Sat | Sun }
-}
--- CSharp_Basics/08-Advanced_CSharp/ConsoleApp2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("something");
-
-var p = new Person("Piter");
-
-var p2 = p with { Name = "ADS"};
-
