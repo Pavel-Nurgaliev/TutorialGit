a5c3d0854f889da2905cb5a3950a7b3a6575db34
Pavel-Nurgaliev <pava62nv@gmial.com>
2026-08-23 15:35:02 +0300

    Inited Vidly project

---
+++ ASP_NET_Fundamentals/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
