9871ce56113d7ac867d0b38baa885c6c1c5b235d
Pavel-Nurgaliev <pava62nv@gmial.com>
2026-09-17 12:02:21 +0300

    Trained returning action results

---
+++ ASP_NET_Fundamentals/VidlyApp/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyApp.Models;
+
+namespace VidlyApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //return View(movie);
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            return new EmptyResult();
+        }
+    }
+}
